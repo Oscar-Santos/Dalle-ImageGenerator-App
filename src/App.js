ee32b4984630525fc465250296cc577acb7cb45e
Oscar Santos <oscar303data@gmail.com>
2023-11-03 16:10:52 -0600

    set up an event-driven interface where users can input a prompt either by clicking an icon or pressing Enter in an input field

--- src/App.js
+++ src/App.js
@@ -1,25 +1,47 @@
-import logo from './logo.svg';
-import './App.css';
+const API_KEY = "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"
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const submitIcon = document.querySelector('#submit-icon')
+const inputElement = document.querySelector('input')
+const imageSection = document.querySelector('.images-section')
+
+const getImages = async () => {
+
+    const options = {
+        method: 'POST',
+        headers: {
+            "Authorization": `Bearer ${API_KEY}`,
+            "Content-Type": "application/json"
+        },
+        body: JSON.stringify( {
+           "prompt": inputElement.value,
+            "n": 4,
+            "size": "1024x1024"
+        })
+    }
+
+    try {
+        const response = await fetch('https://api.openai.com/v1/images/generations', options)
+        const data = await response.json()
+        
+
+        data?.data.forEach((imageObject) => {
+            const imageContainer = document.createElement('div')
+            imageContainer.classList.add('image-container')
+            const imageElement = document.createElement('img')
+            imageElement.setAttribute('src', imageObject.url)
+            imageContainer.append(imageElement)
+            imageSection.append(imageContainer)
+        })
+    } catch (err) {
+        console.error("Error fetching images:", err);
+    }
 }
 
-export default App;
+const handleEnterKeyPress = (event) => {
+    if (event.key === 'Enter') {
+        getImages();
+    }
+};
+
+submitIcon.addEventListener('click', getImages)
+inputElement.addEventListener('keypress', handleEnterKeyPress); 
