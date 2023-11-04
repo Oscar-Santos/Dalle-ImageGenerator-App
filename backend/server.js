e7f0f1cf8c5745a44af77c080ee62f2e7fc7cd6c
Oscar Santos <oscar303data@gmail.com>
2023-11-04 05:39:25 -0600

    - install CORS - fix bug in open api - create env to hide api key

---
+++ backend/server.js
@@ -0,0 +1,40 @@
+
+const express = require('express');
+const cors = require('cors');
+const fetch = require('node-fetch');
+require('dotenv').config();
+
+const app = express();
+const PORT = 3001
+
+app.use(cors());
+app.use(express.json());
+
+app.post('/api/generate-images', async (req, res) => {
+  const { prompt, n, size } = req.body;
+
+  const API_KEY = process.env.API_KEY;
+
+  const options = {
+    method: 'POST',
+    headers: {
+      'Authorization': `Bearer ${API_KEY}`,
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify({ prompt, n, size })
+  };
+
+  try {
+    const response = await fetch('https://api.openai.com/v1/images/generations', options);
+    console.log(response)
+    const data = await response.json();
+    res.json(data);
+  } catch (error) {
+    console.error('Error fetching images:', error);
+    res.status(500).json({ error: 'Internal Server Error' });
+  }
+});
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,47 +1,51 @@
-const API_KEY = "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"
 
-const submitIcon = document.querySelector('#submit-icon')
-const inputElement = document.querySelector('input')
-const imageSection = document.querySelector('.images-section')
+const submitIcon = document.querySelector('#submit-icon');
+const inputElement = document.querySelector('input');
+const imageSection = document.querySelector('.images-section');
 
 const getImages = async () => {
+  const prompt = inputElement.value;
+  const n = 4;
+  const size = '1024x1024';
 
-    const options = {
-        method: 'POST',
-        headers: {
-            "Authorization": `Bearer ${API_KEY}`,
-            "Content-Type": "application/json"
-        },
-        body: JSON.stringify( {
-           "prompt": inputElement.value,
-            "n": 4,
-            "size": "1024x1024"
-        })
+  try {
+    const response = await fetch('http://localhost:3001/api/generate-images', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify({ prompt, n, size })
+    });
+
+    if (!response.ok) {
+      throw new Error(`Error fetching images: ${response.statusText}`);
     }
 
-    try {
-        const response = await fetch('https://api.openai.com/v1/images/generations', options)
-        const data = await response.json()
-        
+    const data = await response.json();
 
-        data?.data.forEach((imageObject) => {
-            const imageContainer = document.createElement('div')
-            imageContainer.classList.add('image-container')
-            const imageElement = document.createElement('img')
-            imageElement.setAttribute('src', imageObject.url)
-            imageContainer.append(imageElement)
-            imageSection.append(imageContainer)
-        })
-    } catch (err) {
-        console.error("Error fetching images:", err);
+    if (!data || !data.data || !Array.isArray(data.data)) {
+      throw new Error('Invalid response format from the API');
     }
-}
 
-const handleEnterKeyPress = (event) => {
-    if (event.key === 'Enter') {
-        getImages();
-    }
+    data.data.forEach((imageObject) => {
+      const imageContainer = document.createElement('div');
+      imageContainer.classList.add('image-container');
+      const imageElement = document.createElement('img');
+      imageElement.setAttribute('src', imageObject.url);
+      imageContainer.append(imageElement);
+      imageSection.append(imageContainer);
+    });
+  } catch (error) {
+    console.error('Error fetching images:', error);
+    // Handle the error appropriately, e.g., display an error message to the user
+  }
 };
 
-submitIcon.addEventListener('click', getImages)
-inputElement.addEventListener('keypress', handleEnterKeyPress); 
+const handleEnterKeyPress = (event) => {
+  if (event.key === 'Enter') {
+    getImages();
+  }
+};
+
+submitIcon.addEventListener('click', getImages);
+inputElement.addEventListener('keypress', handleEnterKeyPress);
